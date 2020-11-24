08a5971245141ae14dce7590c8b2848ceb0e15c6
Wisdowl <arielgoescastro@live.com>
2020-11-24 12:36:27 -0300

    first final version

--- graphs-and-scripts/graph2-AvgTime-UPDATE.p
+++ graphs-and-scripts/graph2-AvgTime-UPDATE.p
@@ -1,16 +1,16 @@
 set encoding iso_8859_1
 set terminal postscript eps enhanced color butt "Times-Roman" 36
-set output 'graph1-AvgDB_avgDELETE.eps'
+set output 'graph2-AvgDB_avgUPDATE.eps'
 
 
 set style data histogram
-set xlabel '# of Rows' font "Times-Roman,42" 
-set ylabel 'Time (ms)' font ",42"
-set size 1.2, 1.0
+set xlabel '# de Linhas' font "Times-Roman,42" 
+set ylabel 'Tempo (ms)' font ",42"
+set size 1.5, 1.2
 set grid ytics
 set style histogram cluster gap 1
 set style fill pattern border -1
-set key left top
+set key right top
 
 set style line 10 lw 3 ps 2.5 lc rgb "#000000"
 set style line 9 lw 3 ps 2.5 lc rgb "#000000"
@@ -23,4 +23,5 @@ set style line 3 lw 3 ps 2.5 lc rgb "#000000"
 set style line 2 lw 3 ps 2.5 lc rgb "#000000" 
 set style line 16 lw 3 ps 2.5 lc rgb "#000000" 
 
-plot 'AVG-DB.out' using 4:xtic(1) title 'InfluxDB' fs pattern 4 lt -1
+plot 'AVG-DB.out' using 3:xtic(1) title 'InfluxDB' fs pattern 4 lt -1, \
+'' using 6:xtic(1) title 'PostgreSQL' fs pattern 2 lt -1
--- graphs-and-scripts/graphs-AvgTime-INSERT-UPDATE-DELETE.sh
+++ graphs-and-scripts/graphs-AvgTime-INSERT-UPDATE-DELETE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for database in InfluxDB #PostgreSQL
+for database in InfluxDB PostgreSQL
 do
 	for size in 1000 10000 100000 1000000 2000000
 	do
@@ -21,24 +21,18 @@ do
 done
 
 
-#string=$(echo "# of Operations IDB-avgINSERT IDB-avgUPDATE IDB-avgDELETE PostgreSQL-avgINSERT PostgreSQL-avgUPDATE PostgreSQL-avgDELETE")
-#echo $string >> AVG-DB.out
 for size in 1000 10000 100000 1000000 2000000
 do
 	allIDB=$(cat avg-time-database-InfluxDB-size-$size.dat)
-	#allPostgreSQL=$(cat avg-time-database-PostgreSQL-size-$size.dat)
+	allPostgreSQL=$(cat avg-time-database-PostgreSQL-size-$size.dat)
 
-	string=$(printf "%-50s %-20s %-20s\n" "$size $allIDB $allPostgreSQL")
+	string=$(echo "$size $allIDB $allPostgreSQL")
 	echo $string >> AVG-DB.out
 
 	rm avg-time-database-InfluxDB-size-$size.dat
 	rm avg-time-database-PostgreSQL-size-$size.dat
 done
 
-printf "%.0e\t%s\t%s\t%s\n" $(cat AVG-DB.out) > AVG-DB.out #format lines
-#awk -i inplace 'BEGINFILE{print "# of Operations IDB-avgINSERT IDB-avgUPDATE IDB-avgDELETE PostgreSQL-avgINSERT PostgreSQL-avgUPDATE PostgreSQL-avgDELETE"}{print}' AVG-DB.out
+printf "%.0e\t%s\t%s\t%s\t%s\t%s\t%s\n" $(cat AVG-DB.out) > AVG-DB.out #format lines
 sed -i '1 i\# of Operations IDB-avgINSERT IDB-avgUPDATE IDB-avgDELETE PostgreSQL-avgINSERT PostgreSQL-avgUPDATE PostgreSQL-avgDELETE\' AVG-DB.out
-#sed -i 's/,/./' AVG-DB.out #replace "," for ".", because printf "%e" uses current locale pt_BR.UTF-8
-sed -i 's/e+/E/' AVG-DB.out 
-
-
+sed -i 's/e+0/E+/' AVG-DB.out
